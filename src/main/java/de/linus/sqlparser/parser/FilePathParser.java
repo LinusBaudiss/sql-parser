cdd010ba64e10866716061302a5ca8cc67ec5e98
linus <l.baudiss@gmx.de>
2019-06-18 21:22:28 +0200

    new filepath parsing - all files under home dir

--- src/main/java/de/linus/sqlparser/parser/FilePathParser.java
+++ src/main/java/de/linus/sqlparser/parser/FilePathParser.java
@@ -5,24 +5,7 @@ import java.util.List;
 
 public class FilePathParser {
 
-	public String createFilePath(String filepath) {
-		String[] inputfiledirectories = filepath.split("/");
-		List<String> filedirectories = new ArrayList<>();
-		StringBuilder filepathBuilder = new StringBuilder();
-		for (String s : inputfiledirectories) {
-			if (!s.isEmpty()) {
-				filedirectories.add(s);
-			}
-		}
-		for (int i = 0; i < filedirectories.size(); i++) {
-			filepathBuilder.append(filedirectories.get(i));
-			if (i == 0) {
-				filepathBuilder.append(":");
-			}
-			if (i != filedirectories.size() - 1) {
-				filepathBuilder.append("\\\\");
-			}
-		}
-		return filepathBuilder.toString();
+	public String createFilePath(String filepath){
+		return System.getProperty("user.home") + filepath;
 	}
 }
